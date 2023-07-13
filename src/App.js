3837b07c6a3f679771c6f8272cb6b6efe9195427
ssumthingood <ssumthingood@gmail.com>
2023-07-13 10:14:35 +0900

    0713 skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Router from "./router/Router";
 
 function App() {
+  // hooks 디렉토리 내에 로컬스토리지의 토큰을 까서 유저 정보 가져오는 훅 생성
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router/>
   );
 }
 
