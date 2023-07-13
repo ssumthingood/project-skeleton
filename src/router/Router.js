1cf4214627ea26d39e04f30e2681157184a9bc46
ssumthingood <ssumthingood@gmail.com>
2023-07-13 11:09:48 +0900

    0713

--- src/router/Router.js
+++ src/router/Router.js
@@ -14,6 +14,11 @@ function Router() {
     const [address, setAdd] = useState('');
     let hello = 'hello';
 
+    const myData = {
+        age:27,
+        blood:'O'
+    }
+
     useEffect(()=>{
         setAdd('서울');
     },[])
@@ -32,7 +37,7 @@ function Router() {
         <Routes>
         {/* Route 컴포넌트를 커스텀해서 로그인해야 들어갈 수 있는 링크인지 아닌지 검증 예정 */}
         {/* components 디렉토리 내에 PublicRoute, AuthRoute 컴포넌트 생성해서 할듯? */}
-        <Route path="/" element={<Main user={myUser} address={address}/>} />
+        <Route path="/" element={<Main user={myUser} address={address} userData={myData}/>} />
         <Route path="/:id" element={<Main user={myUser} address={address}/>} />
         </Routes>
         </BrowserRouter>
